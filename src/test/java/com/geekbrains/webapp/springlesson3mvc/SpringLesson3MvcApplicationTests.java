08d1da2774d9ccf1467ca2aed27cb6aac35100c4
Topsy-Goo <topsygoo@yandex.ru>
2021-07-28 04:40:00 +0300

    Создан проект из https://start.spring.io/
    Проверен на запускаемость.

---
+++ src/test/java/com/geekbrains/webapp/springlesson3mvc/SpringLesson3MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.webapp.springlesson3mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLesson3MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

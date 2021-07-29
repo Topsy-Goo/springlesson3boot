cbb3effe5b020a8ffb6c114d54db821e69b4291f
Topsy-Goo <topsygoo@yandex.ru>
2021-07-29 23:29:53 +0300

    java6.Урок3.«Spring boot» (not MVC) +
    вернули на место методы-примеры, которые не должны были попасть в ДЗ, и переименовали файл-описание в корне.

--- src/main/java/com/geekbrains/webapp/springlesson3mvc/controllers/ProductController.java
+++ src/main/java/com/geekbrains/webapp/springlesson3mvc/controllers/ProductController.java
@@ -1,13 +1,11 @@
 package com.geekbrains.webapp.springlesson3mvc.controllers;
 
+import com.geekbrains.webapp.springlesson3mvc.model.Product;
 import com.geekbrains.webapp.springlesson3mvc.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller     //< наследует от аннотации @Component
 public class ProductController
@@ -21,14 +19,15 @@ public class ProductController
         productService = service;
     }
 
-    //http://localhost:8189/store
+
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store
     @GetMapping
     public String showMainPage()
     {
         return "index";
     }
 
-    //http://localhost:8189/store/showallproducts
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store/showallproducts
     @GetMapping ("/showallproducts")
     public String showAllProducts (Model model) //< Модель помещаем в параметры по мере необходимости.
     {
@@ -36,7 +35,7 @@ public class ProductController
         return "allproducts";
     }
 
-    //http://localhost:8189/store/showproduct/{id}
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store/showproduct/{id}
     @GetMapping ("/product/{id}")
     public String showProduct (@PathVariable Long id, Model model)
     {
@@ -44,7 +43,7 @@ public class ProductController
         return "product";
     }
 
-    //http://localhost:8189/store/create
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store/create
     @GetMapping ("/create")
     public String formProduct (Model model)
     {
@@ -52,6 +51,7 @@ public class ProductController
         return "form";
     }
 
+    //реагирует на POST-запрос с адресом : http://localhost:8189/store/create
     @PostMapping ("/create")
     public String addProduct (@RequestParam String title, @RequestParam String measure,
                               @RequestParam Integer rub, @RequestParam Integer cop,
@@ -65,6 +65,36 @@ public class ProductController
         String msg = ok ? "Товар создан." : "Не удалось создать товар.";
         model.addAttribute ("prompt", msg);
         return "form";
+    }
+
+//------------------ Методы-примеры, не участвующие в приложении: ------------------*/
+
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store/test
+    @GetMapping ("/test")
+    @ResponseBody   //< означает, что возвращаемое значение будет ответом клиенту на его запрос. При отсутствии этой аннотации мы должны вернуть название HTML-страницы.
+    public String getTest()
+    {
+        return "Hello, World!";
+        //Примечательно, что возвращать можно любые объекты, если на странице их есть кому обрабатывать.
+    }
+
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store/product
+    @GetMapping ("/product")
+    @ResponseBody
+    public Product getProduct()
+    {
+        //Если возвращать объект, то он будет перевеёдн в JSON-формат: {"id":1,"title":"сыр","cost":4.5}
+        return new Product ("Кофе зерновой", "250 г", 4.5);
+        //Если возвращать строку, то будет использовать метод Product.toString().
+    }
+
+    //реагирует на GET-запрос с адресом : http://localhost:8189/store/echo?word=Uuuuuu
+    @GetMapping ("/echo")
+    @ResponseBody
+    public String getEchoRequest (@RequestParam (name = "word", defaultValue = "..", required = false) String word)
+    {
+        //(Совместное использование  defaultValue и required бессмысленно и преведено для примера.)
+        return "Echo: "+ word;
     }
 
 }
